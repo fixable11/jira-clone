18e35b3187c8b5b0daa97f18da2610915a5f3981
Mykyta Borovyk <fixable11@gmail.com>
2024-12-03 14:16:09 +0200

    Added workspace settings

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -5,9 +5,10 @@ import { zValidator } from '@hono/zod-validator';
 import { sessionMiddleware } from '@/lib/session-middleware';
 import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from '@/config';
 
-import { createWorkspaceSchema } from '../schemas';
+import { createWorkspaceSchema, updateWorkspaceSchema } from '../schemas';
 import { MemberRole } from '@/features/members/types';
 import { generateInviteCode } from '@/lib/utils';
+import { getMember } from '@/features/members/utils';
 
 const app = new Hono()
   .get('/', sessionMiddleware, async (c) => {
@@ -60,5 +61,69 @@ const app = new Hono()
     });
 
     return c.json({ data: workspace });
-  });
+  })
+  .delete('/:workspaceId', sessionMiddleware, async (c) => {
+    const databases = c.get('databases');
+    const user = c.get('user');
+
+    const { workspaceId } = c.req.param();
+
+    const member = await getMember({
+      databases,
+      workspaceId,
+      userId: user.$id
+    });
+
+    if (!member || member.role !== MemberRole.ADMIN) {
+      return c.json({ error: 'Unauthorized' }, 401);
+    }
+
+    // TODO: Delete members, projects, and tasks
+
+    await databases.deleteDocument(DATABASE_ID, WORKSPACES_ID, workspaceId);
+
+    return c.json({ data: { $id: workspaceId } });
+  })
+  .patch(
+    '/:workspaceId',
+    sessionMiddleware,
+    zValidator('form', updateWorkspaceSchema),
+    async (c) => {
+      const databases = c.get('databases');
+      const storage = c.get('storage');
+      const user = c.get('user');
+
+      const { workspaceId } = c.req.param();
+      const { name, image } = c.req.valid('form');
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id
+      });
+
+      if (!member || member.role !== MemberRole.ADMIN) {
+        return c.json({ error: 'Unauthorized' }, 401);
+      }
+
+      let uploadedImageUrl: string | undefined;
+
+      if (image instanceof File) {
+        const file = await storage.createFile(IMAGES_BUCKET_ID, ID.unique(), image);
+
+        const arrayBuffer = await storage.getFilePreview(IMAGES_BUCKET_ID, file.$id);
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString('base64')}`;
+      } else {
+        uploadedImageUrl = image;
+      }
+
+      const workspace = await databases.updateDocument(DATABASE_ID, WORKSPACES_ID, workspaceId, {
+        name,
+        imageUrl: uploadedImageUrl
+      });
+
+      return c.json({ data: workspace });
+    }
+  );
 export default app;
